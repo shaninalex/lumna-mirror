b014fac745bf7f7714df29153add4b14f6ecfcc5
shaninalex <shanin.x@gmail.com>
2025-08-30 12:12:12 +0300

    chore: file headers

--- tdata/email.go
+++ tdata/email.go
@@ -1,3 +1,5 @@
+// Copyright © 2025 Flowreon https://flowreon.shaninalex.com. All rights reserved.
+
 package tdata
 
 import "gitlab.com/shaninalex/flowreon/internal/email"
--- version.go
+++ version.go
@@ -1,3 +1,5 @@
+// Copyright © 2025 Flowreon https://flowreon.shaninalex.com. All rights reserved.
+
 package main
 
 const Version = "0.4.0"
