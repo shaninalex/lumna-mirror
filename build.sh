b24568690b25d3e552b398835f02862cb38fea5d
shaninalex <shanin.x@gmail.com>
2025-09-14 11:32:56 +0300

    feat(CI): build images

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+TAG=$(git tag --sort=-v:refname | head -n 1)
+
+echo -e "\nLatest Git tag: $TAG"
+
+build_and_push() {
+    local name=$1
+    local dockerfile=$2
+
+    echo -e "\nBUILD: $name"
+    docker build -t flowreon_"$name" -f "$dockerfile" .
+    docker tag flowreon_"$name" shanart/flowreon_"$name":"$TAG"
+    docker push shanart/flowreon_"$name":"$TAG"
+}
+
+build_and_push "auth" "./apps/auth/Dockerfile"
+build_and_push "org" "./apps/org/Dockerfile"
+build_and_push "project" "./apps/project/Dockerfile"
+build_and_push "user" "./apps/user/Dockerfile"
+build_and_push "frontend" "./frontend/Dockerfile"
