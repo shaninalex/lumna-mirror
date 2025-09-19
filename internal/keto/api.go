1cadde2f9fae8157a3fb104a049deae0aeec5e8e
shaninalex <shanin.x@gmail.com>
2025-09-19 09:50:32 +0300

    remove kratos interface

--- internal/keto/api.go
+++
@@ -1,8 +0,0 @@
-// Copyright © 2025 Flowreon https://flowreon.shaninalex.com. All rights reserved.
-
-package keto
-
-// IKeto - i keto.
-type IKeto interface {
-	//GetPermissionsTree(ctx context.Context, userID uuid.UUID) any
-}
