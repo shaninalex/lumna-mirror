e98f99f813340d862ee48cf334d4ec574fb49a82
shaninalex <shanin.x@gmail.com>
2025-09-27 18:58:12 +0300

    docs: project endpoints

--- apps/project/api/controller.go
+++ apps/project/api/controller.go
@@ -21,13 +21,62 @@ func NewProjectController(router *web.Router) {
 
 func (s *ProjectController) init() {
 	projectHandler := handler.NewProjectHandler(domain.NewProjectManagement())
-	s.router.GET("/api/v1/project", projectHandler.HandleProjectsList)
-	s.router.POST("/api/v1/project", projectHandler.HandleProjectCreate)
+
+	// Retrieve all projects
+	s.router.GET("/api/v1/projects", projectHandler.HandleProjectsList)
+	// Create a new project
+	s.router.POST("/api/v1/projects", projectHandler.HandleProjectCreate)
 
 	taskHandler := handler.NewTaskHandler(domain.NewProjectManagement())
-	s.router.POST("/api/v1/project/tasks", taskHandler.HandleTaskCreate)
-	s.router.GET("/api/v1/project/{projectCode}/tasks", taskHandler.HandleProjectTasksList)
-	s.router.GET("/api/v1/project/{projectCode}/tasks/{taskCode}", taskHandler.HandleTaskDetail)
-	s.router.PATCH("/api/v1/project/{projectCode}/tasks/{taskCode}", taskHandler.HandleTaskUpdate)
-	s.router.PATCH("/api/v1/project/{projectCode}/tasks/{taskCode}/status", taskHandler.HandleTaskPatchStatus)
+	s.router.POST("/api/v1/projects/tasks", taskHandler.HandleTaskCreate)
+	s.router.GET("/api/v1/projects/{projectCode}/tasks", taskHandler.HandleProjectTasksList)
+	s.router.GET("/api/v1/projects/{projectCode}/tasks/{taskCode}", taskHandler.HandleTaskDetail)
+	s.router.PATCH("/api/v1/projects/{projectCode}/tasks/{taskCode}", taskHandler.HandleTaskUpdate)
+	s.router.PATCH("/api/v1/projects/{projectCode}/tasks/{taskCode}/status", taskHandler.HandleTaskPatchStatus)
+
+	/*
+
+		GET
+		/api/v1/projects/{id}
+		Retrieve a specific project
+
+		DELETE
+		/api/v1/projects/{id}
+		Delete Project
+
+		PATCH
+		/api/v1/projects/{id}
+		Update specific project
+
+		GET
+		/api/v1/projects/{id}/tasks
+		Retrieve tasks for a project
+
+		POST
+		/api/v1/projects/{id}/tasks
+		Create a new task in a project
+
+		GET
+		/api/v1/project/{id}/badges
+		Retrieve all badges
+
+		POST
+		/api/v1/project/{id}/badges
+		Create project badge
+
+		DELETE
+		/api/v1/project/{id}/badges/{badgeId}
+		Delete project badge
+
+
+
+
+
+
+
+
+
+
+	*/
+
 }
