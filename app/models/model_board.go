58496e611a06fbc3a15dd0e76eae28f13c4079ed
shaninalex <shanin.x@gmail.com>
2025-12-15 00:35:52 +0200

    board list

--- app/models/model_board.go
+++ app/models/model_board.go
@@ -3,7 +3,7 @@ package models
 type Board struct {
 	Id    uint
 	Tasks []*Task
-	Lists []*List
+	Lists []*BoardList
 
 	// settings contains:
 	// - order of lists on frontend
@@ -12,13 +12,3 @@ type Board struct {
 
 func (s *Board) GetId() uint  { return s.Id }
 func (s *Board) SetId(u uint) { s.Id = u }
-
-type List struct {
-	Id   uint
-	Name string
-	// order of list in the board
-	Order uint
-}
-
-func (s *List) GetId() uint  { return s.Id }
-func (s *List) SetId(u uint) { s.Id = u }
---
+++ app/models/model_list.go
@@ -0,0 +1,10 @@
+package models
+
+type BoardList struct {
+	Id    uint
+	Name  string
+	Order uint
+}
+
+func (s *BoardList) GetId() uint  { return s.Id }
+func (s *BoardList) SetId(u uint) { s.Id = u }
