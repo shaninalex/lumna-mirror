de88f9d73231ba0c7db7020b327589362ade4969
shaninalex <shanin.x@gmail.com>
2025-12-25 14:50:19 +0200

    feat(ui): project list view mode

--- webclient/src/app/features/projects-list/projects-list.ts
+++ webclient/src/app/features/projects-list/projects-list.ts
@@ -1,14 +1,25 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import {ProjectStore} from '@entities/project';
 import {ProjectCard} from '@entities/project/ui/project-card/project-card';
+import {NgClass} from '@angular/common';
 
 @Component({
     selector: 'app-projects-list-feature',
-    imports: [ProjectCard],
+    imports: [ProjectCard, NgClass],
     templateUrl: './projects-list.html',
     styleUrl: './projects-list.css',
 })
 export class ProjectsListFeature {
     readonly projectStore = inject(ProjectStore);
     readonly projects = this.projectStore.projects;
+
+    viewMode = signal<"list" | "grid">("grid");
+
+    toggleViewMode(): void {
+        if (this.viewMode() === "list") {
+            this.viewMode.set("grid")
+        } else {
+            this.viewMode.set("list")
+        }
+    }
 }
