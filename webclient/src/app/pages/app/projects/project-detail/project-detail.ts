31145a2b308149dae4efae76d897226b1f613fad
shaninalex <shanin.x@gmail.com>
2025-12-27 08:01:02 +0200

    fix(ui): minor issues #65

--- webclient/src/app/pages/app/projects/project-detail/project-detail.ts
+++ webclient/src/app/pages/app/projects/project-detail/project-detail.ts
@@ -2,15 +2,43 @@ import {Component, computed, effect, inject} from '@angular/core';
 import { ActivatedRoute, RouterLink } from "@angular/router";
 import { ProjectStore } from '@entities/project';
 import { Title } from '@angular/platform-browser';
+import {UiService} from '@shared/ui';
 
 
 @Component({
     selector: 'app-project-detail',
     imports: [RouterLink],
     template: `
-        <h1>Project Detail {{ project()?.name }}</h1>
+        <div class="bg-lime-200 p-4 rounded-xl mb-4">
+            <nav class="flex flex-wrap gap-4">
+                <a href="#" class="hover:underline">Invite members</a>
+                <a href="#" class="hover:underline">Roles</a>
+                <a href="#" class="hover:underline">Webhooks</a>
+                <a href="#" class="hover:underline">Notifications</a>
+                <a href="#" class="hover:underline">Emails</a>
+                <a href="#" class="hover:underline">Integrations</a>
+            </nav>
+        </div>
 
-        <a [routerLink]="['edit']" class="bg-teal-500 text-white rounded-lg px-3 py-1 cursor-pointer inline-block">Edit</a>
+        <div class="bg-gray-200 p-4 rounded-xl mb-4">
+            <h2 class="font-medium text-lg mb-4">Boards</h2>
+            <nav class="flex flex-wrap gap-4">
+                <button class="bg-teal-500 text-white rounded-lg px-4 py-2 cursor-pointer">Development</button>
+                <button class="bg-teal-500 text-white rounded-lg px-4 py-2 cursor-pointer">Design</button>
+                <button class="bg-teal-500 text-white rounded-lg px-4 py-2 cursor-pointer">Marketing/SEO</button>
+            </nav>
+        </div>
+
+        <div class="bg-amber-100 p-4 rounded-xl mb-4">
+            <a class="font-medium text-lg mb-4 hover:underline">Calendar</a>
+        </div>
+
+        <div class="bg-lime-100 p-4 rounded-xl mb-4">
+            <a class="font-medium text-lg mb-4 hover:underline">Documents/Notes</a>
+        </div>
+
+        <a [routerLink]="['edit']"
+           class="bg-teal-500 text-white rounded-lg px-3 py-1 cursor-pointer inline-block">Edit</a>
     `,
 })
 export class ProjectDetail {
@@ -20,7 +48,6 @@ export class ProjectDetail {
 
     private readonly store = inject(ProjectStore);
     project = computed(() => this.store.entities().find(p => p.id === this.projectId));
-
 
     constructor() {
         effect(() => {
--- webclient/src/app/shared/ui/layouts/main/header/header.ts
+++ webclient/src/app/shared/ui/layouts/main/header/header.ts
@@ -1,22 +1,23 @@
-import {Component, inject, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject, Input} from '@angular/core';
 import {UserStore} from '@entities/user';
-import {CdkMenu, CdkMenuItem, CdkMenuTrigger,} from '@angular/cdk/menu';
 import {Dispatcher} from '@ngrx/signals/events';
 import {sessionEvents} from '@core/store/session.store';
+import {UiService} from '@shared/ui';
+import {CdkMenuTrigger} from '@angular/cdk/menu';
 
 @Component({
     selector: 'app-header',
     imports: [
-        CdkMenuTrigger,
-        CdkMenu,
-        CdkMenuItem,
+        CdkMenuTrigger
     ],
     templateUrl: './header.html',
     styleUrl: './header.css',
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Header {
     @Input() title: string
     readonly userStore = inject(UserStore);
+    private ui: UiService = inject(UiService)
     readonly user = this.userStore.user;
     private readonly dispatcher = inject(Dispatcher);
 
