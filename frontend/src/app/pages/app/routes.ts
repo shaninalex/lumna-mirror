cdf300172338d72e96486d032500d4cab238719c
shaninalex <shanin.x@gmail.com>
2026-02-25 21:53:28 +0200

    feat(ui): task detail page

--- frontend/src/app/pages/app/routes.ts
+++ frontend/src/app/pages/app/routes.ts
@@ -5,7 +5,6 @@ import { Home } from './home/home';
 import { authGuard } from './guards/auth.guard';
 import { Calendar } from './calendar/calendar';
 import { DashboardContainer } from './container';
-import { taskRoutes } from './task';
 import { SettingsContainer } from '@pages/app/settings/settings-container';
 import { SettingsPage } from '@pages/app/settings/settings-page/settings-page';
 import { ProjectsContainer } from '@pages/app/projects/container';
@@ -16,9 +15,9 @@ import { ProjectEditPage } from '@pages/app/projects/project-edit/project-edit.p
 import { BoardContainer, BoardPage } from '@pages/app/boards/board-page/board-page';
 import { boardResolver } from '@pages/app/boards/resolver';
 import { BoardEditPage } from '@pages/app/boards/board-edit-page/board-edit-page';
-import {TaskContainer} from '@pages/app/task/container';
-import {taskResolver} from '@pages/app/task/task-resolver';
-import {TaskDetailComponent} from '@pages/app/task/task-detail';
+import { TaskContainer } from '@pages/app/task/container';
+import { taskResolver } from '@pages/app/task/task-resolver';
+import { TaskDetailComponent } from '@pages/app/task/task-detail';
 
 export const routes: Routes = [
     {
@@ -98,8 +97,8 @@ export const routes: Routes = [
                                                 path: ':id',
                                                 component: TaskDetailComponent,
                                                 resolve: {
-                                                    task: taskResolver
-                                                }
+                                                    task: taskResolver,
+                                                },
                                             },
                                         ],
                                     },
@@ -108,7 +107,7 @@ export const routes: Routes = [
                         ],
                     },
                 ],
-            }
+            },
         ],
     },
 ];
