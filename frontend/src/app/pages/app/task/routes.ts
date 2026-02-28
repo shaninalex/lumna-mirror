3e68f373f26f686296e1e2bf92c266f4cea4939f
shaninalex <shanin.x@gmail.com>
2026-02-28 05:40:22 +0200

    dev

--- frontend/src/app/pages/app/task/routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { TaskContainer } from './container';
-import { TaskDetailComponent } from './task-detail';
-import { taskResolver } from '@pages/app/task/task-resolver';
-
-export const taskRoutes: Routes = [
-
-];
