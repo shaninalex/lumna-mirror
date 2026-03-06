4ea9db0fcccb802a8cdb66b2232c77d92de7831b
shaninalex <shanin.x@gmail.com>
2026-03-06 22:54:15 +0200

    feat(ui): invitation entity

---
+++ frontend/src/app/entities/invitation/model/index.ts
@@ -0,0 +1,6 @@
+export type { InvitationState } from './invitation.store';
+export { invitationReducer } from './invitation.store';
+export { InvitationEffects } from './invitation.effects';
+export { selectInvitations } from './invitation.selectors';
+
+export * from './invitation.actions';
