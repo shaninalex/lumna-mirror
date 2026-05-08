3fa68e1f078c089277ccf3996511088ba99c7420
shaninalex <shanin.x@gmail.com>
2026-05-08 07:53:20 +0300

    feat(db): draft workspace table

---
+++ resources/migrations/postgres/20260507084523_workspace.up.sql
@@ -0,0 +1,49 @@
+CREATE TABLE workspaces
+(
+    id         integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title      text      NOT NULL,
+    active     boolean   NOT NULL DEFAULT true,
+    owner_id   integer   NOT NULL REFERENCES identities (id),
+    created_at timestamp NOT NULL DEFAULT NOW(),
+    updated_at timestamp
+);
+
+ALTER TABLE invitations
+    ADD COLUMN workspace_id integer
+        REFERENCES workspaces (id)
+            ON DELETE CASCADE;
+
+CREATE INDEX idx_invitations_workspace_id
+    ON invitations (workspace_id);
+
+ALTER TABLE projects
+    ADD COLUMN workspace_id integer
+        REFERENCES workspaces (id)
+            ON DELETE CASCADE;
+
+CREATE INDEX idx_projects_workspace_id
+    ON projects (workspace_id);
+
+ALTER TABLE boards
+    ADD COLUMN workspace_id integer
+        REFERENCES workspaces (id)
+            ON DELETE CASCADE;
+
+CREATE INDEX idx_boards_workspace_id
+    ON boards (workspace_id);
+
+ALTER TABLE columns
+    ADD COLUMN workspace_id integer
+        REFERENCES workspaces (id)
+            ON DELETE CASCADE;
+
+CREATE INDEX idx_columns_workspace_id
+    ON columns (workspace_id);
+
+ALTER TABLE tasks
+    ADD COLUMN workspace_id integer
+        REFERENCES workspaces (id)
+            ON DELETE CASCADE;
+
+CREATE INDEX idx_tasks_workspace_id
+    ON tasks (workspace_id);
---
+++ resources/migrations/sqlite/20260507084523_workspace.up.sql
@@ -0,0 +1,29 @@
+CREATE TABLE workspaces
+(
+    id         integer PRIMARY KEY AUTOINCREMENT,
+    title      text NOT NULL,
+    active     numeric,
+    owner_id   integer NOT NULL REFERENCES identities(id),
+    created_at datetime DEFAULT CURRENT_TIMESTAMP,
+    updated_at datetime
+);
+
+ALTER TABLE invitations
+    ADD COLUMN workspace_id INTEGER REFERENCES workspaces (id);
+CREATE INDEX idx_invitations_workspace_id ON invitations (workspace_id);
+
+ALTER TABLE projects
+    ADD COLUMN workspace_id INTEGER REFERENCES workspaces (id);
+CREATE INDEX idx_projects_workspace_id ON projects (workspace_id);
+
+ALTER TABLE boards
+    ADD COLUMN workspace_id INTEGER REFERENCES workspaces (id);
+CREATE INDEX idx_boards_workspace_id ON boards (workspace_id);
+
+ALTER TABLE columns
+    ADD COLUMN workspace_id INTEGER REFERENCES workspaces (id);
+CREATE INDEX idx_columns_workspace_id ON columns (workspace_id);
+
+ALTER TABLE tasks
+    ADD COLUMN workspace_id INTEGER REFERENCES workspaces (id);
+CREATE INDEX idx_tasks_workspace_id ON tasks (workspace_id);
