cee0fba56e04518293f47eecf600cb295ab00884
shaninalex <shanin.x@gmail.com>
2026-06-19 09:51:47 +0300

    feat(UI): project backlog task list

--- frontend2/src/app/pages/projects/pages/backlog-page/backlog-page.ts
+++ frontend2/src/app/pages/projects/pages/backlog-page/backlog-page.ts
@@ -7,6 +7,8 @@ import { AsyncPipe } from "@angular/common";
 import { filter, switchMap } from "rxjs/operators";
 import { CardModule } from "primeng/card";
 import { selectTasksByCurrentProject } from "@entities/task";
+import { Listbox } from "primeng/listbox";
+import { FormsModule } from "@angular/forms";
 
 @Component({
     selector: "app-backlog-page",
@@ -15,7 +17,9 @@ import { selectTasksByCurrentProject } from "@entities/task";
         SprintSearchFormBarFeature,
         AsyncPipe,
         TaskInlineFormFeature,
-        CardModule
+        CardModule,
+        Listbox,
+        FormsModule
     ],
     template: `
         <div class="min-h-screen bg-slate-100 text-slate-900">
@@ -29,11 +33,53 @@ import { selectTasksByCurrentProject } from "@entities/task";
 
                 <p-card>
                     @if (tasks$ | async; as tasks) {
-                        <div>
-                            @for (task of tasks; track task.id) {
-                                <div>[{{ task.code }}] - {{ task.title }}</div>
-                            }
-                        </div>
+                        <p-listbox
+                            [options]="tasks"
+                            [(ngModel)]="selectedTask"
+                            (onChange)="onChange($event)"
+                            [striped]="true"
+                            [dragdrop]="true"
+                            [scrollHeight]="''"
+                            optionLabel="title"
+                            optionValue="code"
+                            dataKey="code"
+                            class="mb-4"
+                            style="border: none!important; box-shadow: none!important;"
+                        >
+                            <ng-template #item let-task let-selected="selected">
+                                <div
+                                    class="flex w-full items-center gap-3 py-1"
+                                >
+                                    <i
+                                        class="pi"
+                                        [class.pi-check-circle]="task.done"
+                                        [class.pi-circle]="!task.done"
+                                        [class.text-green-500]="task.done"
+                                        [class.text-slate-400]="!task.done"
+                                    ></i>
+                                    <span
+                                        class="font-mono text-slate-500 shrink-0"
+                                    >
+                                        {{ task.code }}
+                                    </span>
+                                    <span
+                                        class="flex-1 truncate"
+                                        [class.line-through]="task.done"
+                                    >
+                                        {{ task.title }}
+                                    </span>
+                                    <span
+                                        class="rounded px-2 py-0.5 text-[11px] font-semibold uppercase shrink-0"
+                                        [class.bg-green-100]="task.done"
+                                        [class.text-green-700]="task.done"
+                                        [class.bg-slate-200]="!task.done"
+                                        [class.text-slate-600]="!task.done"
+                                    >
+                                        {{ task.done ? "Done" : "To do" }}
+                                    </span>
+                                </div>
+                            </ng-template>
+                        </p-listbox>
                     }
                     <app-task-inline-form-feature />
                 </p-card>
@@ -51,4 +97,9 @@ export class BacklogPage {
     );
 
     tasks$ = this.store.select(selectTasksByCurrentProject);
+    selectedTask?: string;
+
+    onChange(event: any) {
+        console.log(event);
+    }
 }
