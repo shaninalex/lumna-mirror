1d22e5745f3e30a3a4ca16fd933d8d7fbfafc99f
shaninalex <shanin.x@gmail.com>
2026-08-13 07:18:41 +0300

    upd

--- frontend/src/app/entities/task/model/task.selectors.ts
+++ frontend/src/app/entities/task/model/task.selectors.ts
@@ -4,22 +4,9 @@ import { TaskModel } from "./task.model";
 
 const feature = createFeatureSelector<TaskState>("task");
 const selectors = taskAdapter.getSelectors();
-export const selectTaskList = createSelector(feature, (state) =>
-    selectors.selectAll(state)
-);
-
-export const selectTask = (id: number) =>
-    createSelector(selectTaskList, (list) =>
-        list.filter((a: TaskModel) => a.id === id)
-    );
-
+export const selectTaskList = createSelector(feature, (state) => selectors.selectAll(state));
+export const selectTask = (id: number) => createSelector(selectTaskList, (list) => list.filter((a: TaskModel) => a.id === id));
 export const selectTasksByProject = (projectId: number) => createSelector(
     selectTaskList,
-    (list) => {
-        if (!projectId) {
-            return [];
-        }
-
-        return list.filter((a) => a.project_id === projectId);
-    }
+    (list) => (!projectId ? [] : list.filter((a) => a.project_id === projectId))
 );
