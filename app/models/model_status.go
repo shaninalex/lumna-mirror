97026ed37d8b5dd3f9b7849b38007328cd977265
shaninalex <shanin.x@gmail.com>
2026-08-21 11:51:09 +0300

    docs: future model change notes

--- app/models/model_status.go
+++ app/models/model_status.go
@@ -11,7 +11,12 @@ import (
 type Status struct {
 	ID    uint   `gorm:"primaryKey" json:"id"`
 	Title string `gorm:"not null" json:"title"`
-	Order uint   `json:"order"`
+
+	// Order - the order of status (column) in board view of list
+	// NOTE: it make sence only if this view. May be create `meta` column
+	// instead and use this information there? With all other like color,
+	// icon, expanded, collapsed and so on...
+	Order uint `json:"order"`
 
 	ProjectID uint `gorm:"not null;index" json:"project_id"`
 	ListId    uint `gorm:"not null;index" json:"list_id"`
@@ -34,4 +39,11 @@ func (s *Status) BeforeUpdate(tx *gorm.DB) (err error) {
 
 func (s *Status) String() string {
 	return fmt.Sprintf("Column id=%d title=%s", s.ID, s.Title)
+}
+
+type StatusMeta struct {
+	Order    uint   `json:"order"` // NOTE: remove from base model and move here
+	Color    string `json:"color"`
+	Icon     string `json:"icon"`
+	Expanded bool   `json:"expanded"`
 }
